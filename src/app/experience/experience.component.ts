873ce8e0844dc3619266df79217a38742ecb10a4
Kieran Zylstra <kieran.zylstra@colorado.edu>
2019-07-10 21:44:27 -0700

    corrected routing and added some styling

--- src/app/experience/experience.component.ts
+++ src/app/experience/experience.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-experience',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ExperienceComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit() {
+  }
+
+  onClickContact() {
+    this.router.navigate(['contact']);
   }
 
 }
